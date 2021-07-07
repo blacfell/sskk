b7fa32cca37634af48dd05a8961c449133ff7058
kitty fangz <kittycatfangz@gmail.com>
2021-07-07 23:49:25 +0800

    making the struct less terrible

--- main.c
+++ main.c
@@ -1,27 +1,21 @@
 //INCLUDES
 #include <ncurses.h>
 #include <stdlib.h>
+#include <errno.h>
 
 //PROTOTYPES
 static void initialise(void);
 
 //DEFINITIONS
-struct Location {
-    int          x;
-    int          y;
-    struct Item *place;
+struct Objects {
+    const char *name;
+    const char *desc;
+    struct Objects *loc;
+} objs[] = {
+{"you", "the kitty reading this :flushed:", &objs[1]},
+{"start", "the Beginning", NULL},
 };
 
-//this is based on code from this tutorial:
-//https://helderman.github.io/htpataic/htpataic04.html
-struct Item {
-    const  char     *name;
-    const  char     *description;
-    struct Location *loc;
-} items[] = { //this might be hard to understand. it is an array of Items.
-{"start", "the beginning.", NULL},
-{"you", "the kitty reading this :flushed:", {0, 0, &items[0]}},
-};
 
 //get everything set up
 static void initialise(void) {
@@ -34,5 +28,5 @@ static void initialise(void) {
 }
 
 int main(void) {
-
+    initialise();
 }
